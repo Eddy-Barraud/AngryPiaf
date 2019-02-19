950ec9c5433a0f815bf920d96c649cf5d0a9e196
eddydu44 <eddy.barraud@outlook.fr>
2019-02-19 21:25:33 +0100

    Update Angry_Birds_IP.py

--- Angry_Birds_IP.py
+++ Angry_Birds_IP.py
@@ -3,24 +3,24 @@ from math import sqrt
 from math import asin
 
 # Initialisation global pygame
-    pygame.init()
-    surface = pygame.display.set_mode((1400, 500), pygame.RESIZABLE)
-    pygame.display.set_caption("Angry Piaf ;)")
-    clock = pygame.time.Clock()
+pygame.init()
+surface = pygame.display.set_mode((1400, 500), pygame.RESIZABLE)
+pygame.display.set_caption("Angry Piaf ;)")
+clock = pygame.time.Clock()
 
 # Variables globales
-    verif = False
-    inMove = False
-    coord = ()
-    # Sprites Sheets
-    assets = pygame.image.load('image/assets.png').convert_alpha()
+verif = False
+inMove = False
+coord = ()
+# Sprites Sheets
+assets = pygame.image.load('image/assets.png').convert_alpha()
 
 # Sprites Simple
-    bird = assets.subsurface(902, 798, 48, 44)
-    birdCrush = assets.subsurface(904,888, 49, 44)
-    birdCloud = assets.subsurface(908,842,46,44)
-    catapulteAvant = assets.subsurface(833, 0, 43, 126)
-    catapulteArriere = assets.subsurface(0, 0, 38, 200)
+bird = assets.subsurface(902, 798, 48, 44)
+birdCrush = assets.subsurface(904,888, 49, 44)
+birdCloud = assets.subsurface(908,842,46,44)
+catapulteAvant = assets.subsurface(833, 0, 43, 126)
+catapulteArriere = assets.subsurface(0, 0, 38, 200)
 
 
 ###------------------------------------------------------------------------------------------------------------###
